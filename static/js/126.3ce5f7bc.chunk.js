dff90fa63e35d63505ad623147832397ee7d5832
EvgenAltunin <106544836+EvgenAltunin@users.noreply.github.com>
2023-01-30 21:36:53 +0000

    Deploying to gh-pages from @ EvgenAltunin/goit-react-hw-05-movies@4a776c23506264daf8f4bc7396bdbb5a7ed575bc 🚀

---
+++ static/js/126.3ce5f7bc.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[126],{4126:function(e,t,r){r.r(t);var a=r(7689),l=r(1087),n=r(184);t.default=function(e){var t=e.movies,r=(0,a.TH)();return t?(0,n.jsx)("ul",{children:t.map((function(e){var t=e.id,a=e.title,c=e.release_date;return(0,n.jsx)("li",{children:(0,n.jsxs)(l.rU,{to:"/movies/".concat(t),state:{from:r},children:[a," (",c.slice(0,4),")"]})},t)}))}):null}}}]);
+//# sourceMappingURL=126.3ce5f7bc.chunk.js.map
